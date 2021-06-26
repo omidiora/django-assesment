6ec7d89f01f7347aa5c7a75ca8e384cf7c4f1153
omidiora <omidioraemmanuel@gmail.com>
2021-06-26 17:03:59 +0100

    requjirement

--- Task/settings.py
+++ Task/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/2.2/ref/settings/
 """
 
 import os
+import django_heroku
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
@@ -145,3 +146,4 @@ REST_FRAMEWORK = {
 AUTH_USER_MODEL = "UsersAuth.User"
 
 SITE_ID=1
+django_heroku.settings(locals())
